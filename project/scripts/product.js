178b2572cbb57cfaba23ed160b490b0ea5218404
Francillon2024 <fra24017@byui.edu>
2025-03-28 23:38:39 -0400

    add

--- project/scripts/product.js
+++ project/scripts/product.js
@@ -1,4 +1,18 @@
 
+ // Get the current year and populate the #currentyear span in the footer
+ document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+ // Get the document's last modified date and populate the #lastModified paragraph
+ document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menulinks');
+
+hamburgerElement.addEventListener('click', () =>{
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     // Sample data for products (could be fetched from an API)
     const products = [
